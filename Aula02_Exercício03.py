08cf93681d45c661f96497c4934e78b2fd2a2319
Breno Penha dos Santos Gonçalves <breno.psg@gmail.com>
2026-02-19 13:51:33 -0300

    Delete Aula02_Exercício03.py

--- Aula02_Exercício03.py
+++
@@ -1,4 +0,0 @@
-dias = int(input("Quantos dias você esteve com o carro: "))
-km = float(input("Quantos quilometros o carro rodou: "))
-preço = (dias * 60) + (km * 0.15)
-print ("O valor a ser pago é R$ %.2f" % preço )
