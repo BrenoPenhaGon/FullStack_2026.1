5c9c379c2583b1e1f5af585c74bc68e665e9e32b
Breno Penha dos Santos Gonçalves <breno.psg@gmail.com>
2026-03-11 17:03:05 -0300

    Introdução JavaScript

---
+++ Aulas/Aula 05/script.js
@@ -0,0 +1,39 @@
+// console.log("Olá Classe!")
+// console.log("Exemplo")
+
+// let nome = prompt("Digite o seu nome:")
+// console.log(nome)
+// window.alert("Seu nome é " + nome)
+
+// let i = 0;
+// while(i<10){
+//     window.alert(i);
+//     i = i+1;
+// }
+
+// for(let i=0; i <10; i = i+1){
+//     window.alert(i)
+// } 
+
+// console.log("Atividade 1: ")
+
+// for(let i = 1; i <= 100; i = i+2){
+//     if(i%2 != 0){
+//         console.log(i)
+//     }
+// }
+
+// console.log("Atividade 2: ")
+
+for(let n = 0; n <= 500; n = n+5){
+    if(n%2 !=0){
+        console.log(n)
+    }   
+}
+
+let número = prompt("Digite um número")
+for(let número = 0; número >= 0; número = número-1){
+    if(número%2 !=0){
+        console.log(número)
+    }
+}
