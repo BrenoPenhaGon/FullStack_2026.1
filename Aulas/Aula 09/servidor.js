908abbf5f44d708141916dd21d8d5127cd6a2b2d
Breno Penha dos Santos Gonçalves <breno.psg@gmail.com>
2026-04-29 15:16:50 -0300

    Servidor get e post

--- Aulas/Aula 09/servidor.js
+++ Aulas/Aula 09/servidor.js
@@ -1,12 +1,38 @@
 require("colors")
 var http = require("http")
 var express = require("express")
+var bodyParser = require("body-parser")
 
 var app = express()
 
 app.use(express.static("./public"))
+app.use(bodyParser.urlencoded({extended: false }))
+app.use(bodyParser.json())
+
 
 var server = http.createServer(app)
 server.listen(80)
 
 console.log("Servidor rodando ...". rainbow)
+
+app.get("/", function(req, res){
+    res.redirect("index.html")
+})
+
+app.get("/inicio", function(req, res){
+    var text = req.query.text;
+    var number = req.query.number;
+    var color = req.query.color;
+
+    console.log(text, number, color)
+    console.log("Requisição feita por GET")
+})
+
+app.post("/inicio", function(req, res){
+    var text = req.body.text;
+    var number = req.body.number;
+    var color = req.body.color;
+
+    console.log(text, number, color)
+    console.log("Requisição feita por POST")
+})
